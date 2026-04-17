08fdc27dd3fa1306bfa7a6d92f3397dc07be50c0
TladiKekana <tladikarts@gmail.com>
2026-04-17 14:22:39 +0200

    set-properties

---
+++ properties/propertyForSaleArr.js
@@ -0,0 +1,30 @@
+export const propertyForSaleArr = [
+    {
+        propertyLocation: "Bloemspruit, Bloemfontein",
+        priceGBP: 890000,
+        roomsM2: [14, 18, 14, 10, 6],
+        comment: "Highly desirable location in stunning scenery!",
+        image: "../images/bloemspruit.jpg"
+    },
+    {
+        propertyLocation: "Kasi, Gemiston",
+        priceGBP: 690000,
+        roomsM2: [12, 16, 12, 8, 6],
+        comment: "very chilled neighbourhood!",
+        image: "../images/gemiston.jpg"
+    },
+    {
+        propertyLocation: "Moreleta-Park, Pretoria",
+        priceGBP: 1200000,
+        roomsM2: [16, 18, 16, 12, 8],
+        comment: "Very beautiful area!",
+        image: "../images/moreleta-park.jpg"
+    },
+    {
+        propertyLocation: "Westonaria, Easten-Cape",
+        priceGBP: 990000,
+        roomsM2: [15, 13, 15, 10, 7],
+        comment: "Great place to raise a family!",
+        image: "../images/westonaria.jpg"
+    }
+]
